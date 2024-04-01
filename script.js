763ac9b6dcbd0cd8bc572658dfee0b6a7ad82f77
Jacob-2-13 <johandrismolinaipuana@gmail.com>
2024-03-31 19:11:47 -0500

    script inicial

---
+++ script.js
@@ -0,0 +1,13 @@
+let myNumber = 10;
+let myString = "Hola, mundo";
+let myBoolean = true;
+let myObject = {
+nombre: "ChatGPT",
+version: "4",
+};
+let myNull = null;
+let myUndefined;
+console.log(myNumber + myString); // ¿Qué ocurre
+aquí?
+console.log(myObject.nombre): // ¿Y aquí?
+console.log(myObject.apellido); // ¿Y en este caso?
