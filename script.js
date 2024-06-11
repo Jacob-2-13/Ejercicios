a53f02adbcb1474846f7a642e108a29fe3383dac
Jacob-2-13 <johandrismolinaipuana@gmail.com>
2024-06-11 14:56:22 -0500

    funcion suma

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myNumber = 10;
+/*let myNumber = 10;
 let myString = "Hola, mundo";
 let myBoolean = true;
 let myObject = {
@@ -10,4 +10,11 @@ let myUndefined;
 console.log(myNumber + myString); // ¿Qué ocurre
 aquí?
 console.log(myObject.nombre): // ¿Y aquí?
-console.log(myObject.apellido); // ¿Y en este caso?
+console.log(myObject.apellido); // ¿Y en este caso?*/
+
+function suma(num1,num2){
+    result = num1 + num2;
+    return result;
+}
+
+console.log(suma(4,5));
